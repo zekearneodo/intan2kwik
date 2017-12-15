a6b65b301191c1faa823ab49a977eca42ec95125
Zeke Arneodo <zeke.arneodo@gmail.com>
2017-12-14 17:05:20 -0800

    - added support for the rhs format (RHS2000 system for recording/stimulating)  - fixed crashing when no adc/dig in was present.

--- intan2kwik/core/reading.py
+++ intan2kwik/core/reading.py
@@ -3,16 +3,50 @@ import logging
 import glob
 import os
 
-from intan2kwik.core.intan.util.read_header import read_header
-from intan2kwik.core.intan.load_intan import read_data
+from intan2kwik.core.intan.util.read_header import read_header as read_header_rhd
+from intan2kwik.core.intan_rhs.util.read_header import read_header as read_header_rhs
+
+from intan2kwik.core.intan.load_intan import read_data as read_data_rhd
+from intan2kwik.core.intan_rhs.load_intan import read_data as read_data_rhs
 
 logger = logging.getLogger('intan2kwik.core.reading')
+rh_search_string = '*.rh[d, s]'
 
-
+# a clunky wrapper for read_intan_header that selects the right version
 def read_intan_header(file_path):
+    """
+    :param file_path (string): file path (ends with .rhs/.rhd)
+    :return: header structure, corresponding to the rhs/rhd file specification
+    """
+    ext = os.path.split(file_path)[-1].split('.')[-1]
     with open(file_path, 'rb') as f:
-        hdr = read_header(f)
+        if ext == 'rhs':
+            logger.debug('Entered RHS file')
+            hdr = read_header_rhs(f)
+        elif ext == 'rhd':
+            logger.debug('Entered RHD file')
+            hdr = read_header_rhd(f)
+        else:
+            raise Exeption('Unrecognized extension {}'.format(ext))
     return hdr
+
+
+def read_data(file_path):
+    """
+    :param file_path (string): file path (ends with .rhs/.rhd)
+    :return: header structure, corresponding to the rhs/rhd file specification
+    """
+    ext = os.path.split(file_path)[-1].split('.')[-1]
+    if ext == 'rhs':
+        logger.debug('Entered RHS file for reading data')
+        data = read_data_rhs(file_path)
+    elif ext == 'rhd':
+        logger.debug('Entered RHD file for reading data')
+        data = read_data_rhd(file_path)
+    else:
+        raise Exeption('Unrecognized extension {}'.format(ext))
+
+    return data
 
 
 def s_f_lookup(sec_name, intan_data):
@@ -21,8 +55,10 @@ def s_f_lookup(sec_name, intan_data):
 
 def native_to_board(native_name, names_dict=None):
     if names_dict is None:
+        #short name of the channel: name of the group of channels in the header dictionary
         names_dict = {'adc': 'board_adc',
-                      'din': 'board_dig_in'}
+                      'din': 'board_dig_in',
+                      'dac': 'board_dac'}
     name, number = native_name.split('-')
     return names_dict[name]
 
@@ -30,6 +66,7 @@ def native_to_board(native_name, names_dict=None):
 def native_to_t(native_name, names_dict=None):
     if names_dict is None:
         names_dict = {'adc': 't_board_adc',
+                      'dac': 't_board_adc',
                       'din': 't_dig'}
     name, number = native_name.split('-')
     return names_dict[name]
@@ -78,13 +115,13 @@ def read_aux(native_name, intan_data, volt=True, intan_header=None):
 def read_intan_rec(rec_folder):
     logger.info('reading intan rec {}'.format(rec_folder))
     # list all the chunk files:
-    all_rhd_files = glob.glob(os.path.join(rec_folder, '*.rhd'))
-    all_rhd_files.sort()
-    logger.info('Found {} chunks'.format(len(all_rhd_files)))
+    all_rhx_files = glob.glob(os.path.join(rec_folder, rh_search_string))
+    all_rhx_files.sort()
+    logger.info('Found {} chunks'.format(len(all_rhx_files)))
 
     all_intan_rec = []
 
-    for i_file, rhd_file in (enumerate(all_rhd_files)):
+    for i_file, rhd_file in (enumerate(all_rhx_files)):
         all_intan_rec.append(read_data(rhd_file))
 
     return all_intan_rec
@@ -94,7 +131,7 @@ def read_aux_all_rec(native_name_list, rec_folder, volt=True):
     # go through all the records extracting the one channel (very inefficient)
     # it reads all the block for the record
     logger.info('reading intan chans {0} across all of rec {1}'.format(native_name_list, rec_folder))
-    all_rhd_files = glob.glob(os.path.join(rec_folder, '*.rhd'))
+    all_rhd_files = glob.glob(os.path.join(rec_folder, rh_search_string))
     all_rhd_files.sort()
     logger.info(all_rhd_files)
     # list all the chunk files:
--- intan2kwik/kwd.py
+++ intan2kwik/kwd.py
@@ -4,10 +4,9 @@ import glob
 import os
 import h5py
 
-from intan2kwik.core.intan.util.read_header import read_header
-from intan2kwik.core.intan.load_intan import read_data
 from intan2kwik.core.h5 import tables
 from intan2kwik.core import reading
+#from intan2kwik.core.reading import rh_search_string
 
 logger = logging.getLogger('intan2kwik.kwd')
 
@@ -48,7 +47,7 @@ def list_chan_bit_volts(header, include_channels):
 
 
 def rhd_data_block(rhd_file, include_chans, times_too=False):
-    read_block = read_data(rhd_file)
+    read_block = reading.read_data(rhd_file)
     # identify channels, make numy array with all the block and a list of metadata for each
     block_data = np.vstack([read_block['{}_data'.format(ch_grp)] for ch_grp in include_chans])
     block_t = np.vstack([read_block['t_{}'.format(ch_grp)] for ch_grp in include_chans])
@@ -61,11 +60,19 @@ def rhd_data_block(rhd_file, include_chans, times_too=False):
     return ret_value
 
 
-def rhd_rec_to_table(rhd_list, parent_group, include_chans):
+def rhd_rec_to_table(rhd_list, parent_group, chan_groups_wishlist):
+    '''
+    :param rhd_list: (list of strings) paths of files to include in this rec
+    :param parent_group: (h5 object group) parent group for this rec
+    :param chan_groups_wishlist: (flat ndarray/list) channel groups to get from the rhd files
+    :return:
+    '''
     # make the table
     # read the blocks and append them to the table
     # set the attributes of the table
-    logger.info('Appending {} files to data table in {}'.format(len(rhd_list), parent_group))
+    logger.info('Appending {} files to data table in {}'.format(len(rhd_list),
+                                                                parent_group))
+
 
     total_samples_in = 0
     total_samples_in_dig =0
@@ -74,19 +81,32 @@ def rhd_rec_to_table(rhd_list, parent_group, include_chans):
     s_f = parent_group.attrs.get('sample_rate')
 
     for i, rhd_file in enumerate(rhd_list):
-        read_block = read_data(rhd_file)
-
+        read_block = reading.read_data(rhd_file)
+        if i==0:
+            # filter include groups, warn if a group wasn't in the data and remove it from the list
+            include_chan_groups = []
+            for g in chan_groups_wishlist:
+                if '{}_data'.format(g) in read_block.keys():
+                    include_chan_groups.append(g)
+                else:
+                    logger.warn('Channel data group {} is not in the recordings')
         # The main data (neural chans and dac chans; from include_chans)
-        block_data = np.vstack([read_block['{}_data'.format(ch_grp)] for ch_grp in include_chans])
-        block_t = np.vstack([read_block['t_{}'.format(ch_grp)] for ch_grp in include_chans])
+        block_data = np.vstack([read_block['{}_data'.format(ch_grp)] for ch_grp in include_chan_groups])
+        block_t = np.vstack([read_block['t_{}'.format(ch_grp)] for ch_grp in include_chan_groups])
         save_block = block_data.T.astype(np.int32) - 32768
         # assuming the first block in include_chans is amplifier
         save_t = block_t[0].reshape([-1, 1])
-        name_t = 't_{}'.format(include_chans[0])
+        name_t = 't_{}'.format(include_chan_groups[0])
 
         # The digital channels
-        dig_in_data = read_block['board_dig_in_data'].T.astype(np.short)
-        dig_in_t = read_block['t_dig'].reshape([-1, 1])
+
+        try:
+            dig_in_data = read_block['board_dig_in_data'].T.astype(np.short)
+            dig_in_t = read_block['t_dig'].reshape([-1, 1])
+            has_digital_in = True
+        except KeyError as k:
+            logger.warn('No digital channels')
+            has_digital_in = False
 
         # data comes in as uint16
         if i == 0:
@@ -95,16 +115,19 @@ def rhd_rec_to_table(rhd_list, parent_group, include_chans):
                                               save_block.astype(np.int16))
             tset = tables.unlimited_rows_data(parent_group, name_t, save_t)
 
-            logger.info('Creating tables of digital data')
-            dset_dig = tables.unlimited_rows_data(parent_group, 'dig_in',
-                                                  dig_in_data.astype(np.short))
-            tset_dig = tables.unlimited_rows_data(parent_group, 't_dig', dig_in_t)
+            if has_digital_in:
+                logger.info('Creating tables of digital data')
+                dset_dig = tables.unlimited_rows_data(parent_group, 'dig_in',
+                                                      dig_in_data.astype(np.short))
+                tset_dig = tables.unlimited_rows_data(parent_group, 't_dig', dig_in_t)
+
         else:
             tables.append_rows(dset, save_block.astype(np.int16))
             tables.append_rows(tset, save_t)
 
-            tables.append_rows(dset_dig, dig_in_data.astype(np.short))
-            tables.append_rows(tset_dig, dig_in_t)
+            if has_digital_in:
+                tables.append_rows(dset_dig, dig_in_data.astype(np.short))
+                tables.append_rows(tset_dig, dig_in_t)
 
             # assert time continuity
             more_control_d_samples = (save_t[0] * s_f - total_samples_in)
@@ -119,12 +142,15 @@ def rhd_rec_to_table(rhd_list, parent_group, include_chans):
         last_t = save_t[-1]
         total_samples_in += save_block.shape[0]
 
-        last_t_dig = dig_in_t[-1]
-        total_samples_in_dig += last_t_dig.shape[0]
+        if has_digital_in:
+            last_t_dig = dig_in_t[-1]
+            total_samples_in_dig += last_t_dig.shape[0]
 
     # only atrribute for table is valid_samples
     dset.attrs.create('valid_samples', np.ones(save_block.shape[1]) * total_samples_in)
-    dset_dig.attrs.create('valid_samples', np.ones(dig_in_data.shape[1]) * total_samples_in_dig)
+
+    if has_digital_in:
+        dset_dig.attrs.create('valid_samples', np.ones(dig_in_data.shape[1]) * total_samples_in_dig)
 
 def create_data_grp(rec_grp, intan_hdr, include_channels, rec):
     logger.debug('Creating data group for this rec {}'.format(rec))
@@ -153,7 +179,15 @@ def create_data_grp(rec_grp, intan_hdr, include_channels, rec):
     return data_grp
 
 
-def intan_to_kwd(folder, dest_file_path, rec=0, include_channels=None):
+def intan_to_kwd(folder, dest_file_path, rec=0, include_channels=None, board='rhd'):
+    """
+    :param folder: (string) folder where the .rh? files are
+    :param dest_file_path: (string) dest of the kw? files
+    :param rec: (int)
+    :param include_channels: (flat ndarray/list)
+    :param board: (str) 'rhd' or 'rhs' for rhd2000 or rhs2000
+    :return:
+    """
     # make the .kwd file
     # make the /recording/0 group
     # dump header to application data
@@ -161,13 +195,14 @@ def intan_to_kwd(folder, dest_file_path, rec=0, include_channels=None):
     if include_channels is None:
         include_channels = ['amplifier', 'board_adc']
     logger.info('reading intan chans data across all of rec {0}'.format(folder))
-    all_rhd_files = glob.glob(os.path.join(folder, '*.rhd'))
-    all_rhd_files.sort()
-    logger.info('Found {} files to process'.format(len(all_rhd_files)))
+
+    all_rhx_files = glob.glob(os.path.join(folder, '*.{}'.format(board)))
+    all_rhx_files.sort()
+    logger.info('Found {} .{} files to process'.format(len(all_rhx_files), board))
 
     # attributes from the header
-    first_header = reading.read_intan_header(all_rhd_files[0])
-
+    first_header = reading.read_intan_header(all_rhx_files[0])
+    # logger.info('First header {}'.format(first_header))
     v_multipliers = [0.195, 50.354e-6]
     if first_header['eval_board_mode'] == 1:
         v_multipliers[1] = 152.59e-6
@@ -175,13 +210,13 @@ def intan_to_kwd(folder, dest_file_path, rec=0, include_channels=None):
     # chan_names =
 
     # list all the chunk files:
-
+    logger.debug('dest file: {}'.format(dest_file_path))
     with h5py.File(dest_file_path, 'a') as kwd_file:
         logger.debug('Creating the /recordings/{}'.format(rec))
         rec_grp = kwd_file.require_group('recordings')
         data_grp = create_data_grp(rec_grp, first_header, include_channels, rec)
         # create application data with all metadata
         logger.debug('Creating data table and going throug the recs')
-        rhd_rec_to_table(all_rhd_files, data_grp, include_channels)
+        rhd_rec_to_table(all_rhx_files, data_grp, include_channels)
 
     return first_header
